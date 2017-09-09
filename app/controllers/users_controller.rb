b623abf04aa8d5e978bf84fd01c8c9b3c3ca8de7
t-kusakabe <ttmac0704@gmail.com>
2017-09-10 05:41:50 +0900

    modify user controller model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def new
     @user = User.new
+    @prefectures = YAML.load_file('prefectures.yml').keys
   end
 
   def create
@@ -10,6 +11,12 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :birthday, :real_type, :image)
+    parse_prefecture(params.require(:user)[:location])
+    params.require(:user)[:location] = parse_prefecture(params.require(:user)[:location])
+    params.require(:user).permit(:name, :birthday, :real_type, :location, :image)
+  end
+
+  def parse_prefecture(prefecture)
+    a = YAML.load_file('prefectures.yml')[prefecture]
   end
 end
